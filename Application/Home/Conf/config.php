2611d2cefd7ff346649bbf0e2be3d53972e958eb
DESKTOP-2BA7CGR\Aeop <q214353047@qq.com>
2018-09-01 15:39:08 +0800

    1da

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
 	//数据库配置信息
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_HOST'   => 'localhost', // 服务器地址s
 	'DB_NAME'   => 'dbhelop', // 数据库名
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => '123456', // 密码
