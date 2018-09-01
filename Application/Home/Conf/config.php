b2445586d6e6bfb0ddae818136afc8a40c95a1f3
DESKTOP-2BA7CGR\Aeop <q214353047@qq.com>
2018-09-01 15:35:34 +0800

    1da

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -4,7 +4,7 @@ return array(
 	//数据库配置信息
 	'DB_TYPE'   => 'mysql', // 数据库类型
 	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'my', // 数据库名
+	'DB_NAME'   => 'dbhelop', // 数据库名
 	'DB_USER'   => 'root', // 用户名
 	'DB_PWD'    => '123456', // 密码
 	'DB_PORT'   => 3306, // 端口
